0c5c21f7ae37f0de36232daf1b91e47f5d838306
EricLi <1057635338@qq.com>
2017-02-18 10:44:47 +0800

    【新增】新增api文件

--- demo1/js/weather/province_list.js
+++ demo1/js/weather/province_list.js
@@ -8,10 +8,11 @@ import {
 	ListView
 } from 'react-native';
 
-import CityList from './city_list'
+import CityList from './city_list';
+import API from './api';
 
 export default class ProvinceList extends Component {
-	
+
 	constructor(props){
 		super(props);
 		const ds = new ListView.DataSource({rowHasChanged:(r1,r2)=>(r1 !== r2)});
@@ -19,29 +20,31 @@ export default class ProvinceList extends Component {
 			dataSource:ds.cloneWithRows(this._getRows())
 		};
 	}
-	
-	
+
+
 	render(){
 		return(
-			<ListView 
+			<ListView
 				dataSource={this.state.dataSource}
 				renderRow={this._renderRow.bind(this)}
 			/>
 		);
 	}
-	
-	
+
+
 	_getRows(){
+		var response = API.getPovince();
+		console.log(response);
 		const dataBlob = ['北京市','天津市','河北省','山西省','内蒙古自治区','辽宁省','吉林省','黑龙江省','上海市','江苏省','浙江省','安徽省','福建省','江西省','山东省','河南省','湖北省','湖南省','广东省','广西壮族自治区'];
 		return dataBlob;
 	}
-	
+
 	_renderRow(rowData,sectionID,rowID){
 		return(
 			<Text onPress={this._onPress.bind(this,rowData,sectionID,rowID)}>{rowData}</Text>
 		);
 	}
-	
+
 	_onPress(rowData,sectionID,rowID){
 		const {navigator} = this.props;
 		if(navigator){
