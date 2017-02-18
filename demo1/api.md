8e1d5ba675040126c1b3dedbb799439fb97b5257
EricLi <1057635338@qq.com>
2017-02-18 08:51:48 +0800

    【新增】新增api文档

---
+++ demo1/api.md
@@ -0,0 +1,11 @@
+城市查询一级接口：
+http://guolin.tech/api/china
+
+城市查询二级接口
+http://guolin.tech/api/china/一级接口的返回省代码
+
+城市查询三级接口
+http://guolin.tech/api/china/一级接口的返回省代码/二级查询返回的市代码
+
+天气查询主接口,其中吧，cityid就是三级查询到的weather_id,key就是和风天气的key
+http://guolin.tech/api/weather?cityid=?&key=?
--- demo1/index.ios.js
+++ demo1/index.ios.js
@@ -1,53 +1,146 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
 
+'use strict';
+// var SearchPage = require('./js/SearchPage');
+import ViewLayout from './js/ViewLayout';
 import React, { Component } from 'react';
 import {
   AppRegistry,
   StyleSheet,
   Text,
+  Navigator,
+  TextInput,
+  Image,
+  TouchableOpacity,
   View
 } from 'react-native';
 
-export default class demo1 extends Component {
+class SearchHouse extends Component {
+
+  configureScene(route, routeStack) {
+    if (route.type == 'Modal') {
+      return Navigator.SceneConfigs.FloatFromBottom;
+    }
+    return Navigator.SceneConfigs.PushFromRight;
+  }
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
+      <Navigator
+        style={{flex: 1}}
+        initialRoute={{
+          title: 'Property Finder',
+          index: 0,
+          component: ViewLayout
+        }}
+        configureScene={this.configureScene}
+        navigationBar={
+          <Navigator.NavigationBar
+            style={styles.navContainer}
+            routeMapper={NavigationBarRouteMapper}/>
+        }
+        renderScene={(route, navigator)=>{
+          let Com = route.component;
+          //传递navigator下去
+          return <Com {...route.passProps} navigator={navigator}/>
+        }}
+        />
     );
   }
 }
 
-const styles = StyleSheet.create({
+var NavigationBarRouteMapper = {
+  LeftButton(route, navigator,index,  navState){
+      if (index > 0){
+        return (
+          <View style={styles.navContainer}>
+            <TouchableOpacity
+              underlayColor='transparent'
+              onPress={()=>{if(index>0){navigator.pop()}}}>
+              <Text style={styles.leftNavButtonText}>
+                Back
+              </Text>
+            </TouchableOpacity>
+          </View>
+        )
+      }else {
+        return null;
+      }
+  },
+  // 右键
+ RightButton(route, navigator, index, navState) {
+   if (route.onPress)
+     return (
+       <View style={styles.navContainer}>
+         <TouchableOpacity
+           onPress={() => route.onPress()}>
+           <Text style={styles.rightNavButtonText}>
+             {route.rightText || '右键'}
+           </Text>
+         </TouchableOpacity>
+       </View>
+     );
+ },
+ // 标题
+ Title(route, navigator, index, navState) {
+   return (
+     <View style={styles.navContainer}>
+       <Text style={styles.title}>
+          {route.title}
+       </Text>
+     </View>
+   );
+ }
+};
+
+
+
+var styles = StyleSheet.create({
+
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  navContainer: {
+    backgroundColor: '#48BBEC',
+    paddingTop: 12,
+    paddingBottom: 10,
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  headText: {
+    color: '#ffffff',
+    fontSize: 22
   },
+  button: {
+    height: 60,
+    marginTop: 10,
+    justifyContent: 'center', // 内容居中显示
+    backgroundColor: '#ff1049',
+    alignItems: 'center'
+  },
+  buttonText: {
+    fontSize: 18,
+    color: '#ffffff'
+  },
+// 左面导航按钮
+leftNavButtonText: {
+  color: '#ffffff',
+  fontSize: 18,
+  marginLeft: 13
+},
+// 右面导航按钮
+rightNavButtonText: {
+  color: '#ffffff',
+  fontSize: 18,
+  marginRight: 13
+},
+// 标题
+title: {
+  width: 200,//不指定大小就不行，我纳闷了
+  fontSize: 18,
+  color: '#fff',
+  justifyContent: 'center',
+  textAlign: 'center',
+  fontWeight: 'bold',
+  flex: 1
+},
 });
 
 AppRegistry.registerComponent('demo1', () => demo1);
