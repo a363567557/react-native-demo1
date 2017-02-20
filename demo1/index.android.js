bb76e6bf918c6a498ae563d00359849be77d9b10
a363567 <363567557@qq.com>
2017-02-20 17:46:25 +0800

    去掉无用代码

--- demo1/index.android.js
+++ demo1/index.android.js
@@ -1,45 +1,16 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
-import React, { Component } from 'react';
+import React from 'react';
 import {
   AppRegistry,
-  StyleSheet,
-  Text,
-  View,
-  Navigator
 } from 'react-native';
 
 import WeatherComponent from './js/weather/index.weather';
 
-export default class demo1 extends Component {
+export default class demo1 extends React.Component {
   render() {
     return (
       	<WeatherComponent />
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
 
 AppRegistry.registerComponent('demo1', () => demo1);
